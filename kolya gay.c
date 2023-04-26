ff744dd45e621b1484de60df4b7947dca0c589ce
LIMONN12 <toyman098@gmail.com>
2023-04-26 03:00:46 +0300

    Gay

---
+++ kolya gay.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(void)
+{
+    for (int zalop = 0; zalop < 40; zalop++)
+    {
+        printf("gay\n");
+        if (zalop % 10 == 0)
+        {
+            printf("kolya\n");
+        }
+    }
+}
